caf8ed0868cb2f6a73a48266b18cb9647f22a34e
FrancoisGraux <frsgra28@gmail.com>
2023-03-26 12:58:37 +0200

    modification de style + card prject fini

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const header = document.querySelector(".site-header");
+const cards = document.querySelectorAll(".projectCard");
 let oldScrollY = document.body.scrollTop;
 
-
+cards.forEach(card=>{
+    let check = false;
+    card.querySelector("button")?.addEventListener("click",()=>{
+        if (check === false){
+            card.querySelector(".cardBody").style.translate = "100% 0";
+        } else {
+            card.querySelector(".cardBody").style.translate = "";
+        }
+        check = !check
+    })
+})
 document.addEventListener("scroll",()=>{
     if (oldScrollY<=0){
         header.classList.remove('hide');
